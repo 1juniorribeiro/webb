4e60e1fdf1cd5c2ddf0d678709c9bf498f67f7cc
Amauri Junior <junior_18ribeiro@hotmail.com>
2020-10-13 21:48:40 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//JSX JavaScript XML
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
